10065d6fad54dffd75efbdf5bd10d112ad6fe791
Egor <leskovegor490@gmail.com>
2025-06-27 10:16:26 +0300

    update

--- index.js
+++ index.js
@@ -3,27 +3,36 @@ const copyBtn = document.getElementById('copy-btn')
 document.addEventListener('keydown', (event) => {
 	if (event.code === 'KeyC') {
 		CopyTextInBuffer()
+	} else {
+		funcHelp()
 	}
 })
 
 function LogMessageShowAndClear(message) {
 	const logMessageParagraph = document.getElementById('log-message')
+	if (logMessageParagraph) {
+		logMessageParagraph.textContent = message
 
-	logMessageParagraph.textContent = message
+		setTimeout(() => {
+			logMessageParagraph.textContent = ''
+		}, 2000)
+	}
+}
 
-	setTimeout(() => {
-		logMessageParagraph.textContent = ''
-	}, 2000)
+function funcHelp() {
+	const bodyHtml = document.getElementById('body')
+	bodyHtml.innerHTML = ''
 }
 
 function CopyTextInBuffer() {
 	const copyText = document.getElementById('copy-text')
-
-	navigator.clipboard.writeText(copyText.textContent)
-		.then(() => {
-			LogMessageShowAndClear('Текст скопирован!')
-		})
-		.catch(() => {
-			LogMessageShowAndClear(`Ошибка в копировании текста`)
-		})
+	if (copyText) {
+		navigator.clipboard.writeText(copyText.textContent)
+			.then(() => {
+				LogMessageShowAndClear('Текст скопирован!')
+			})
+			.catch(() => {
+				LogMessageShowAndClear(`Ошибка в копировании текста`)
+			})
+	}
 }
